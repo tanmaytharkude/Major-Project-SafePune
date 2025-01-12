3b79f77af28a7d8dc0c8b590f7ac19fae7c12e74
tanmaytharkude <tanmaytharkude29@gmail.com>
2025-01-12 13:42:33 +0530

    Add firebaseConfig.js back to the repository

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require("firebase/app");
+const { getDatabase, ref, set, get, child } = require("firebase/database");
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+module.exports = { database, ref, set, get, child };
